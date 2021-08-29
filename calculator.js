1706639d4cbc7efb4eadc29ed81c09fa46dc4efd
samyukthapeeyems <samyukthapeeyems@gmail.com>
2021-08-30 01:06:06 +0530

    added transition to the buttons

--- calculator.js
+++ calculator.js
@@ -1,11 +1,7 @@
 function display(num){
-    // var result=document.getElementById("result").value;
-    // result += num;
-    
     document.getElementById("result").value+=num;
-
-
 }
+
 function solve(){
     var solution=document.getElementById("result").value;
     var res = eval(solution);
@@ -13,6 +9,5 @@ function solve(){
 }
 
 function erase(){
-    
     document.getElementById("result").value="";
 }
